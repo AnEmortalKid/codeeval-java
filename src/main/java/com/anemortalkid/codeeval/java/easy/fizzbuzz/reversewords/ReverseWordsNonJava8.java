be659921ad68d1a338cba908eb71bc80e4c8331b
Jan Monterrubio <Jan.Monterrubio@Cerner.com>
2016-01-31 14:44:46 -0600

    adding nonjava8 solution

---
+++ src/main/java/com/anemortalkid/codeeval/java/easy/fizzbuzz/reversewords/ReverseWordsNonJava8.java
@@ -0,0 +1,26 @@
+package com.anemortalkid.codeeval.java.easy.fizzbuzz.reversewords;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class ReverseWordsNonJava8 {
+
+	public static void main(String[] args) {
+		try {
+			List<String> lines = Files.readAllLines(Paths.get(args[0]));
+			for (String line : lines) {
+				String[] words = line.split(" ");
+				StringJoiner joiner = new StringJoiner(" ");
+				for (String word : words) {
+					joiner.add(word);
+				}
+				System.out.println(joiner.toString());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
